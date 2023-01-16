eb8a3cc6c96e599332541f51b86dcf7f17dc1b3f
Domenico Paolo Picariello <picariellopaolo224@gmail.com>
2023-01-16 15:29:08 +0100

    Milestone 1 completa

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,34 +6,36 @@ export default {
         return {
             store,
             apiKey: 'd5245d1c7515f7cf0f5e303f27c6a28a',
-            apiUrl: 'https://api.themoviedb.org/3/search/movie?'
+            apiUrl: 'https://api.themoviedb.org/3/search/movie'
         }
     },
 
     methods: {
-        getMovies() {
+        getMovies(querySearch) {
             axios.get(this.apiUrl, {
                 params: {
-                    api_Key: this.apiKey,
-                    query: 'ladri',
+                    api_key: this.apiKey,
+                    query: querySearch,
                 }
             })
                 .then((response) => {
-                    console.log(response);
+                    // console.log(response.data.results);
+                    this.store.moviesLists = response.data.results;
+                    console.log(this.store.moviesLists)
                 })
         }
     },
 
     created() {
-        this.getMovies();
+        // this.getMovies();
     }
 }
 </script>
 
 <template>
     <header>
-        <input type="text">
-        <button>Search</button>
+        <input type="text" v-model="store.querySearch">
+        <button @click="getMovies(store.querySearch)">Search</button>
     </header>
 </template>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,36 @@
 <script>
-export default {
+import { store } from '../store.js'
 
+export default {
+    data() {
+        return {
+            store,
+        }
+    }
 }
 </script>
 
 <template>
-    
+    <main>
+        <ul>
+            <li v-for="movie in store.moviesLists">
+                <h4>
+                    {{ movie.title }}
+                </h4>
+                <h5>
+                    {{ movie.original_title }}
+                </h5>
+                <p>
+                    {{ movie.original_language }}
+                </p>
+                <p>
+                    {{ movie.vote_average }}
+                </p>
+            </li>
+        </ul>
+    </main>
 </template>
 
 <style lang="scss" scoped>
-    
+
 </style>
