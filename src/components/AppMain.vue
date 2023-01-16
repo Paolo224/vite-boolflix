bc0f1ac6b9fde473b7297d2a4d3c7d739e658165
Domenico Paolo Picariello <picariellopaolo224@gmail.com>
2023-01-16 15:49:22 +0100

    Bandiere della Milestone 2 completa

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -20,8 +20,20 @@ export default {
                 <h5>
                     {{ movie.original_title }}
                 </h5>
-                <p>
-                    {{ movie.original_language }}
+                <p v-if="(movie.original_language == 'it')">
+                    <img src="../assets/italia.png" alt="">
+                </p>
+                <p v-else-if="(movie.original_language == 'en')">
+                    <img src="../assets/inghilterra.png" alt="">
+                </p>
+                <p v-else-if="(movie.original_language == 'fr')">
+                    <img src="../assets/francia.png" alt="">
+                </p>
+                <p v-else-if="(movie.original_language == 'es')">
+                    <img src="../assets/spagna.png" alt="">
+                </p>
+                <p v-else>
+                    {{ movie.original_language.toUpperCase() }}
                 </p>
                 <p>
                     {{ movie.vote_average }}
